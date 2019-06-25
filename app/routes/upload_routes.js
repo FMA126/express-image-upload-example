bee5c6631c16244f573daa32ccd7e19a7955d6fb
FMA126 <mhblanks@gmail.com>
2019-06-25 10:26:38 -0400

    Use multer to add req.file object
    - Use multer middleware to put file back together on the server side.
        - It stores the file in a temporary location/

--- app/routes/upload_routes.js
+++ app/routes/upload_routes.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const Upload = require('../models/upload')
+const multer = require('multer')
+const multerUpload = multer({ dest: 'tempFiles/' })
+const { promiseReadFile, createParams, s3Upload } = require('../../lib/promiseS3Upload')
+
+console.log(promiseReadFile)
 
 const router = express.Router()
 
 // CREATE
 // POST /examples
-router.post('/upload', (req, res, next) => {
+router.post('/uploads', multerUpload.single('file'), (req, res, next) => {
+  console.log(req.file)
   Upload.create(req.body.upload)
     // respond to succesful `create` with status 201 and JSON of new "upload"
     .then(upload => {
